6b5e84e735d974af625f8e018d7d3899e35001cc
deenr <deenrayman@outlook.com>
2020-12-27 12:13:36 +0100

    flush kut ding

--- src/main/java/be/kuleuven/csa/MainDatabase.java
+++ src/main/java/be/kuleuven/csa/MainDatabase.java
@@ -1,18 +1,23 @@
 package be.kuleuven.csa;
 
+import be.kuleuven.csa.domain.Auteur;
 import be.kuleuven.csa.domain.AuteurRepository;
 import be.kuleuven.csa.jdbi.AuteurRepositoryJdbi3Impl;
+import be.kuleuven.csa.jdbi.ConnectionManager;
 import org.jdbi.v3.core.Jdbi;
 import org.jdbi.v3.sqlobject.SqlObjectPlugin;
 import org.lightcouch.CouchDbClient;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 
 public class MainDatabase {
     private static AuteurRepository auteurRepository;
 
     private static void setUpRepo() {
-        var jdbi = Jdbi.create("jdbc:sqlite:csa_database.db");
+        ConnectionManager connectionManager = new ConnectionManager();
+        connectionManager.flushConnection();
+        var jdbi = Jdbi.create(ConnectionManager.ConnectionString);
         jdbi.installPlugin(new SqlObjectPlugin());
 
         auteurRepository = new AuteurRepositoryJdbi3Impl(jdbi);
@@ -23,6 +28,11 @@ public class MainDatabase {
         //CouchDbClient couchDbClient = new CouchDbClient();
         setUpRepo();
 
+        Auteur auteur = new Auteur("Pierertje");
+
+        auteurRepository.saveNewAuteur(auteur);
+
+        System.out.println(auteurRepository.getAllAuteurs().toString());
 
     }
 }
---
+++ src/main/java/be/kuleuven/csa/jdbi/ConnectionManager.java
@@ -0,0 +1,50 @@
+package be.kuleuven.csa.jdbi;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+    private Connection connection;
+    public static final String ConnectionString = "jdbc:sqlite:csa_database.db";
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void flushConnection() {
+        try {
+            connection.commit();
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public ConnectionManager() {
+        try {
+            // auto-creates if not exists
+            connection = DriverManager.getConnection(ConnectionString);
+            connection.setAutoCommit(false);
+
+            initTables();
+        } catch (Exception e) {
+            System.out.println("Db connection handle failure");
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void initTables() throws Exception {
+        var sql = new String(Files.readAllBytes(Paths.get("D:\\Coding\\DAB\\CSA_Application\\src\\main\\resources\\dbcreate.sql")));
+        System.out.println(sql);
+
+        var s = connection.createStatement();
+        s.executeUpdate(sql);
+        s.close();
+    }
+
+
+}
