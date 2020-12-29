504d3b3b0a6d5331deead21963b57e748c0e7f75
deenr <deenrayman@outlook.com>
2020-12-29 04:33:52 +0100

    LIST TIPS FILTER FUNCTIE

--- src/main/java/be/kuleuven/csa/controller/TipsMainController.java
+++ src/main/java/be/kuleuven/csa/controller/TipsMainController.java
@@ -3,17 +3,28 @@ package be.kuleuven.csa.controller;
 import be.kuleuven.csa.domain.helpdomain.DataVoorAbonnementenTableView;
 import be.kuleuven.csa.domain.helpdomain.Tip;
 import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.collections.FXCollections;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import org.lightcouch.CouchDbClient;
 
-import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 public class TipsMainController {
-    public TableView<Tip> tips_table;
+    public List<Tip> tipList;
 
-    public void initialize() throws IOException {
+    public TableView<Tip> tips_table;
+    public ChoiceBox<String> filterProductSoort_choice;
+    public Button applyFilterProductSoort_button;
+    public Button resetFilterProductSoort_button;
+
+    public void initialize() {
+        loadDb();
+        applyFilterProductSoort_button.setOnAction(e -> applyFilterProductSoort());
+        resetFilterProductSoort_button.setOnAction(e -> resetFilterProductSoort());
 
         tips_table.getColumns().clear();
 
@@ -32,21 +43,56 @@ public class TipsMainController {
         TableColumn<Tip, String> colURL = new TableColumn<>("Tip");
         colURL.setCellValueFactory(f -> new ReadOnlyObjectWrapper<>(f.getValue().getTip_url()));
         tips_table.getColumns().add(colURL);
-
-
-        refreshTable();
     }
 
-    private void refreshTable() {
+    private void applyFilterProductSoort() {
         tips_table.getItems().clear();
-        CouchDbClient dbClient = new CouchDbClient();
+        for (Tip tip : tipList) {
+            if (filterProductSoort_choice.getSelectionModel().getSelectedItem().equals("Alles")) {
+                tips_table.getItems().add(tip);
+            } else if (filterProductSoort_choice.getSelectionModel().getSelectedItem().equals("Groenten")) {
+                if (tip.getProduct_soort().equals("Groenten")) {
+                    tips_table.getItems().add(tip);
+                }
+            } else if (filterProductSoort_choice.getSelectionModel().getSelectedItem().equals("Fruit")) {
+                if (tip.getProduct_soort().equals("Fruit")) {
+                    tips_table.getItems().add(tip);
+                }
+            } else if (filterProductSoort_choice.getSelectionModel().getSelectedItem().equals("Vlees")) {
+                if (tip.getProduct_soort().equals("Vlees")) {
+                    tips_table.getItems().add(tip);
+                }
+            } else if (filterProductSoort_choice.getSelectionModel().getSelectedItem().equals("Bloemen")) {
+                if (tip.getProduct_soort().equals("Bloemen")) {
+                    tips_table.getItems().add(tip);
+                }
+            }
+        }
+    }
 
-        List<Tip> tipList = dbClient.view("_all_docs").includeDocs(true).query(Tip.class);
+    private void resetFilterProductSoort() {
+        filterProductSoort_choice.setValue("Alles");
+        insertInToTable();
+    }
 
-        dbClient.shutdown();
-        
+    private void insertInToTable() {
+        tips_table.getItems().clear();
+
+        List<String> productSoorten = Arrays.asList("Alles", "Groenten", "Fruit", "Vlees", "Bloemen");
+        filterProductSoort_choice.setItems(FXCollections.observableArrayList(productSoorten));
+        if (filterProductSoort_choice.getSelectionModel().getSelectedItem() == null) {
+            filterProductSoort_choice.setValue("Alles");
+        }
+
         for (Tip tip : tipList) {
             tips_table.getItems().add(tip);
         }
+    }
+
+    private void loadDb() {
+        CouchDbClient dbClient = new CouchDbClient();
+        this.tipList = dbClient.view("_all_docs").includeDocs(true).query(Tip.class);
+        dbClient.shutdown();
+        insertInToTable();
     }
 }
